4f7a41b8735a9c79bde4041e9149a456cc9eae76
DanaBaraban <danabaraban2408@gmail.com>
2018-12-25 15:18:07 +0200

    Register implementation

--- app/src/main/java/com/decompany/theworker/MainActivity_HR.java
+++ app/src/main/java/com/decompany/theworker/MainActivity_HR.java
@@ -5,14 +5,69 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity_HR extends AppCompatActivity {
+    private Button dayOffButton;
+    private Button salaryRaiseButton;
+    private Button sickNoteButton;
+    private Button createStatisticsButton;
+    private Button createNewUserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_hr);
+
+        dayOffButton = (Button) findViewById(R.id.dayOff);
+        salaryRaiseButton = (Button) findViewById(R.id.salaryRaise);
+        sickNoteButton = (Button) findViewById(R.id.sickNote);
+        createStatisticsButton = (Button) findViewById(R.id.createStatistics);
+        createNewUserButton = (Button) findViewById(R.id.createNewUser);
+
+//        dayOffButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), VacationActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        salaryRaiseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity_HR.this, SalaryRaiseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        sickNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity_HR.this, SickNoteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        createStatisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity_HR.this, StatisticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        createNewUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity_HR.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
@@ -35,5 +90,11 @@ public class MainActivity_HR extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void kkk(View view){
+        Intent intent = new Intent(this, VacationActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/decompany/theworker/SignupActivity.java
+++ app/src/main/java/com/decompany/theworker/SignupActivity.java
@@ -1,9 +1,8 @@
-/*
 package com.decompany.theworker;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,150 +11,150 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
-    private static final String TAG = "SignupActivity";
 
-    @BindView(R.id.input_name) EditText _nameText;
-    @BindView(R.id.input_department) EditText _addressText;
-    @BindView(R.id.input_email) EditText _emailText;
-    @BindView(R.id.input_mobile) EditText _mobileText;
-    @BindView(R.id.input_password) EditText _passwordText;
-    @BindView(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
-    @BindView(R.id.btn_signup) Button _signupButton;
-    //@BindView(R.id.link_login) TextView _loginLink;
-    
+    private EditText nameText;
+    private EditText emailText;
+    private EditText passwordText;
+    private EditText reEnterPasswordText;
+    private EditText departmentText;
+    private EditText linkToPhotoText;
+    private EditText mobileText;
+    private EditText managerNameText;
+    private EditText positionText;
+    private EditText kindOfEmployeeText;
+    private EditText salaryText;
+
+    private Button signupButton;
+
+    private FirebaseAuth mAuth;
+    private String currentUserId;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        ButterKnife.bind(this);
 
-        _signupButton.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        nameText = findViewById(R.id.input_name);
+        emailText = findViewById(R.id.input_email);
+        passwordText = findViewById(R.id.input_password);
+        reEnterPasswordText = findViewById(R.id.input_reEnterPassword);
+        departmentText = findViewById(R.id.input_department);
+        linkToPhotoText = findViewById(R.id.link_to_photo);
+        mobileText = findViewById(R.id.input_mobile);
+        managerNameText = findViewById(R.id.manager_name);
+        positionText = findViewById(R.id.input_position);
+        kindOfEmployeeText = findViewById(R.id.input_kind_of_employee);
+        salaryText = findViewById(R.id.input_salary);
+        signupButton = findViewById(R.id.btn_signup);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signup();
-            }
-        });
+                //AllowUserToSignup();
 
-        _loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Finish the registration screen and return to the Login activity
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
-                startActivity(intent);
-                finish();
-                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
-            }
-        });
-    }
+                final String name = nameText.getText().toString();
+                final String position = positionText.getText().toString();
+                final String email = emailText.getText().toString();
+                final String password = passwordText.getText().toString();
+                final String reEnterPassword = reEnterPasswordText.getText().toString();
+                final String mobile = mobileText.getText().toString();
+                final String managerName = managerNameText.getText().toString();
+                final String department = departmentText.getText().toString();
+                final String linkToPhoto = linkToPhotoText.getText().toString();
+                final String kindOfEmployee = kindOfEmployeeText.getText().toString();
+                final String salary = salaryText.getText().toString();
 
-    public void signup() {
-        Log.d(TAG, "Signup");
+                validate(name, position, email, password, reEnterPassword, mobile, managerName, department, linkToPhoto, kindOfEmployee, salary);
 
-        if (!validate()) {
-            onSignupFailed();
-            return;
-        }
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        currentUserId = mAuth.getCurrentUser().getUid();
+                                        final Employee e = new Employee(currentUserId, name, position, email, mobile, managerName, department, linkToPhoto, password, kindOfEmployee, salary);
+                                        FirebaseDatabase.getInstance().getReference().child("workers").child(currentUserId).setValue(e).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                            if (task.isSuccessful()) {
+                                                                Toast.makeText(SignupActivity.this, "New user created", Toast.LENGTH_SHORT).show();
+                                                                finish();
+                                                            } else {
+                                                                String message = task.getException().toString();
+                                                                Toast.makeText(SignupActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                                                            }
+                                                        }
+                                                    });
+                                                }
 
-        _signupButton.setEnabled(false);
+                                            }});}});}
 
-        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
-                R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
-
-        String name = _nameText.getText().toString();
-        String address = _addressText.getText().toString();
-        String email = _emailText.getText().toString();
-        String mobile = _mobileText.getText().toString();
-        String password = _passwordText.getText().toString();
-        String reEnterPassword = _reEnterPasswordText.getText().toString();
-
-        // TODO: Implement your own signup logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
+                    public void onSignupSuccess () {
+                        signupButton.setEnabled(true);
+                        setResult(RESULT_OK, null);
+                        finish();
                     }
-                }, 3000);
-    }
 
+                    public void onSignupFailed () {
+                        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
 
-    public void onSignupSuccess() {
-        _signupButton.setEnabled(true);
-        setResult(RESULT_OK, null);
-        finish();
-    }
+                        signupButton.setEnabled(true);
+                    }
+                    public boolean validate (String name, String position, String email, String password, String reEnterPassword, String mobile, String managerName, String department, String linkToPhoto, String kindOfEmployee, String salary ){
+                        boolean valid = true;
 
-    public void onSignupFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+                        if (name.isEmpty() || name.length() < 3) {
+                            nameText.setError("at least 3 characters");
+                            valid = false;
+                        } else {
+                            nameText.setError(null);
+                        }
 
-        _signupButton.setEnabled(true);
-    }
+                        if (department.isEmpty()) {
+                            departmentText.setError("Enter Valid Department");
+                            valid = false;
+                        } else {
+                            departmentText.setError(null);
+                        }
 
-    public boolean validate() {
-        boolean valid = true;
+                        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                            emailText.setError("enter a valid email address");
+                            valid = false;
+                        } else {
+                            emailText.setError(null);
+                        }
 
-        String name = _nameText.getText().toString();
-        String address = _addressText.getText().toString();
-        String email = _emailText.getText().toString();
-        String mobile = _mobileText.getText().toString();
-        String password = _passwordText.getText().toString();
-        String reEnterPassword = _reEnterPasswordText.getText().toString();
+                        if (mobile.isEmpty() || mobile.length() != 10) {
+                            mobileText.setError("Enter Valid Mobile Number");
+                            valid = false;
+                        } else {
+                            mobileText.setError(null);
+                        }
 
-        if (name.isEmpty() || name.length() < 3) {
-            _nameText.setError("at least 3 characters");
-            valid = false;
-        } else {
-            _nameText.setError(null);
-        }
+                        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+                            passwordText.setError("between 4 and 10 alphanumeric characters");
+                            valid = false;
+                        } else {
+                            passwordText.setError(null);
+                        }
 
-        if (address.isEmpty()) {
-            _addressText.setError("Enter Valid Address");
-            valid = false;
-        } else {
-            _addressText.setError(null);
-        }
+                        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+                            reEnterPasswordText.setError("Password Do not match");
+                            valid = false;
+                        } else {
+                            reEnterPasswordText.setError(null);
+                        }
 
-
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
-            valid = false;
-        } else {
-            _emailText.setError(null);
-        }
-
-        if (mobile.isEmpty() || mobile.length()!=10) {
-            _mobileText.setError("Enter Valid Mobile Number");
-            valid = false;
-        } else {
-            _mobileText.setError(null);
-        }
-
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            _passwordText.setError(null);
-        }
-
-        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
-            _reEnterPasswordText.setError("Password Do not match");
-            valid = false;
-        } else {
-            _reEnterPasswordText.setError(null);
-        }
-
-        return valid;
-    }
-}
-*/
+                        return valid;
+                    }
+                }
