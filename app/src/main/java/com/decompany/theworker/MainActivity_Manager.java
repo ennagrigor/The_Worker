eeee2003b0632ccef97d569a24933d028df43e7a
DanaBaraban <danabaraban2408@gmail.com>
2018-12-27 14:28:29 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/decompany/theworker/MainActivity_HR.java

--- app/src/main/java/com/decompany/theworker/MainActivity_Manager.java
+++ app/src/main/java/com/decompany/theworker/MainActivity_Manager.java
@@ -5,14 +5,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity_Manager extends AppCompatActivity {
+
+    private Button publishAmessageBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_manager);
+
+        publishAmessageBtn =findViewById(R.id.publishAMessage);
+
+        publishAmessageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity_Manager.this, PublishMessage.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
